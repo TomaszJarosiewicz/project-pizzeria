bd84e543cbc3cb1556b8c71d68a97cddd9756d24
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-08-16 21:53:49 +0200

    Download data with API

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,12 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    // CODE ADDED DB
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
@@ -447,22 +453,34 @@
     initMenu: function(){
       const thisApp = this;
 
-      for(let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+      for(let productData in thisApp.data.products){
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+          thisApp.data.products = parsedResponse;
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };

df550bab8f322bc605cb69beafaa6ae34d1081a2
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-08-20 21:37:39 +0200

    Add method initPages

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import { Product } from './components/Product.js';
 import { Cart } from './components/Cart.js';
-import { select, settings } from './settings.js';
+import { select, settings, classNames } from './settings.js';
 
 const app = {
   initCart: function(){
@@ -42,9 +42,41 @@ const app = {
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        console.log('Clicked Id', id);
+        thisApp.activePage(id);
+      });
+    }
+    thisApp.activePage(thisApp.pages[0].id);
+  },
+
+  activePage: function(pageId){
+    const thisApp = this;
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+      console.log('Link', link);
+    }
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.nav.active, page.id == pageId);
+      console.log('Page', page);
+    }
+  },
+
   init: function(){
     const thisApp = this;
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },

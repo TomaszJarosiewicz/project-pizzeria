ddf15e522fa2af1af7774cc4f218857927695269
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-08-25 08:42:46 +0200

    Change date for repeat false

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { select, templates, settings } from '../settings.js';
+import { select, templates } from '../settings.js';
 import { utils } from '../utils.js';
 import { AmountWidget } from './AmountWidget.js';
 import { DatePicker } from './DatePicker.js';
